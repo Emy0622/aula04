d53523d733d72a3eb832e5e8f2eb01b9eba80ec4
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2023-08-23 12:18:38 -0300

    7/7 arrumar botoes

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ const botaoMostrarNumerosMultiplos3 = document.getElementById('botao-mostrar-num
 const botaoMostrarNumerosMultiplos3e4 = document.getElementById('botao-mostrar-numeros-multiplos-3-4')
 const botaoMostrarNumerosMultiplos3ou4 = document.getElementById('botao-mostrar-numeros-multiplos-3-ou-4')
 const botaoMostrarDobro = document.getElementById('botao-mostrar-dobro')
+const botaoMostrarProximoNumero = document.getElementById('botao-mostrar-proximo-numero')
+const botaoMostrarProximoNumeroPar = document.getElementById('botao-mostrar-proximo-numero-par')
 
 
 botaoMostrarTodosNumeros.addEventListener('click', mostrarTodosNumeros)
@@ -15,8 +17,9 @@ botaoMostrarNumerosImpares.addEventListener('click', mostrarNumerosImpares)
 botaoMostrarNumerosMultiplos3.addEventListener('click',mostrarNumerosMultiplos3)
 botaoMostrarNumerosMultiplos3e4.addEventListener('click',mostrarNumerosMultiplos3e4)
 botaoMostrarNumerosMultiplos3ou4.addEventListener('click',mostrarNumerosMultiplos3ou4)
-
 botaoMostrarDobro.addEventListener('click',mostrarDobro)
+botaoMostrarProximoNumero.addEventListener('click',mostrarProximoNumero)
+botaoMostrarProximoNumeroPar.addEventListener('click',mostrarProximoNumeroPar)
 
 function mostrarTodosNumeros (){
     const numeros = document.getElementById('numeros').value.split(',')
@@ -90,7 +93,8 @@ function mostrarNumerosMultiplos3e4 (){
     const ultimoIndice = numeros.length
 
     for (let indice=0; indice < ultimoIndice; indice++){
-        if (numeros[indice] % 3 == 0){
+        if (numeros[indice] % 3 == 0 && numeros[indice] % 4 == 0){
+
 
             const novoSpan = document.createElement('span')
             novoSpan.textContent = numeros[indice]
@@ -108,7 +112,7 @@ function mostrarNumerosMultiplos3ou4 (){
     const ultimoIndice = numeros.length
 
     for (let indice=0; indice < ultimoIndice; indice++){
-        if (numeros[indice] % 3 == 0){
+        if (numeros[indice] % 3 == 0 || numeros[indice] % 4 == 0){
             const novoSpan = document.createElement('span')
             novoSpan.textContent = numeros[indice]
             container.appendChild(novoSpan)
@@ -124,13 +128,48 @@ function mostrarDobro (){
     const ultimoIndice = numeros.length
 
     for (let indice=0; indice < ultimoIndice; indice++){
-        if (numeros[indice] % 3 == 0){
+
             const novoSpan = document.createElement('span')
-            novoSpan.textContent = numeros[indice]
+            novoSpan.textContent = Number(numeros[indice]) * 2
+            container.appendChild(novoSpan)
+    }
+}
+
+function mostrarProximoNumero (){
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-mostrar-proximo-numero')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for (let indice=0; indice < ultimoIndice; indice++){
+
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = Number(numeros[indice]) + 1
+            container.appendChild(novoSpan)
+    }
+}
+
+function mostrarProximoNumeroPar (){
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-proximo-numero-par')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for (let indice=0; indice < ultimoIndice; indice++){
+        if (numeros[indice] % 2 == 0){
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = Number(numeros[indice]) + 2
+            container.appendChild(novoSpan)
+        } else if (numeros[indice] % 2 != 0){
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = Number(numeros[indice]) + 1
             container.appendChild(novoSpan)
         }
     }
-}
+   
+    }
 
 
 
